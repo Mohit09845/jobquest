912082b257bb4ca7f2962fea92e3ee6069198f07
Mohit Sharma <sharmaamohit121@gmail.com>
2025-04-09 21:55:08 +0530

    uploadthing, companyForm almost finished

---
+++ components/forms/CompanyForm.tsx
@@ -0,0 +1,170 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { companySchema } from "@/app/utils/zodSchema";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { countryList } from "@/app/utils/countriesList";
+import { Textarea } from "../ui/textarea";
+import { UploadDropzone } from "../general/UploadThingReexported";
+
+export function CompanyForm() {
+  const form = useForm<z.infer<typeof companySchema>>({
+    resolver: zodResolver(companySchema),
+    defaultValues: {
+      about: "",
+      location: "",
+      logo: "",
+      name: "",
+      website: "",
+      xAccount: "",
+    },
+  });
+  return (
+    // {...form} passes all useForm() methods (like register, handleSubmit, control, etc.) into the component.
+
+    <Form {...form}>
+      <form className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter company Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Location</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select Location" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Worldwide</SelectLabel>
+                      <SelectItem value="worldwide">
+                        <span>🌍</span> <span>Worldwide / Remote</span>
+                      </SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Location</SelectLabel>
+                      {countryList.map((country) => (
+                        <SelectItem key={country.code} value={country.name}>
+                          <span>{country.flagEmoji}</span>
+                          <span className="pl-2">{country.name}</span>
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="website"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Website</FormLabel>
+                <FormControl>
+                  <Input placeholder="https://yourcompany.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="xAccount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>X (Twitter) Account</FormLabel>
+                <FormControl>
+                  <Input placeholder="@yourcompany" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="about"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>About</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us about your company..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="logo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Company Logo</FormLabel>
+              <UploadDropzone
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                  field.onChange(res[0].url);
+                }}
+                onUploadError={() => {
+                  console.log("Something went wrong");
+                }}
+                className="ut-button:bg-primary ut-button:text-white ut-button:hover:bg-primary/90 ut-label:text-muted-foreground ut-allowed-content:text-muted-foreground border-primary"
+              />
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
+
+// <FormMessage/> is designed to display validation error messages for the field it's placed inside.
+// inside render we're destructuring field, which comes from React Hook Form's Controller under the hood, and it contains all the necessary props to connect your input to the form state.
